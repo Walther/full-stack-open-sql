7aab3962a79bda55c2aec062073242f0cddcbc7c
Walther <veeti.haapsamo@gmail.com>
2023-01-15 00:37:46 +0200

    Solve exercise 13.16

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ const { connectToDatabase } = require("./util/db");
 const blogRouter = require("./controllers/blog");
 const userRouter = require("./controllers/user");
 const loginRouter = require("./controllers/login");
+const authorRouter = require("./controllers/author");
 const errorHandler = require("./middlewares/errorHandler");
 
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/authors", authorRouter);
+
 app.use(errorHandler);
 
 const start = async () => {
