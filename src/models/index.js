529fb39d1542d51846c24c6653ce5e5d78c1ef31
Walther <veeti.haapsamo@gmail.com>
2023-01-15 13:16:16 +0200

    Solve exercise 13.19

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,12 @@
 const Blog = require("./blog");
+const Readinglist = require("./readinglist");
 const User = require("./user");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
+
+User.belongsToMany(Blog, { through: Readinglist });
+Blog.belongsToMany(User, { through: Readinglist });
 
 module.exports = {
   Blog,
