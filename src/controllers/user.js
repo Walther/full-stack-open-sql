759478c4e8516146abc11c1fe06d75e656f9a149
Walther <veeti.haapsamo@gmail.com>
2023-01-15 14:34:37 +0200

    Solve exercise 13.23

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,32 +2,17 @@ const router = require("express").Router();
 
 const { User, Blog, Readinglist } = require("../models");
 const tokenExtractor = require("../middlewares/tokenExtractor");
+const { Op } = require("sequelize");
 
-const userOptions = {
-  include: [
-    {
+router.get("/", async (_req, res) => {
+  const users = await User.findAll({
+    include: {
       model: Blog,
       attributes: {
         exclude: ["userId"],
       },
     },
-    {
-      model: Blog,
-      as: "reading_list",
-      attributes: { exclude: ["userId"] },
-      through: {
-        attributes: ["id", "read"],
-        as: "status",
-      },
-      include: {
-        model: User,
-        attributes: ["name"],
-      },
-    },
-  ],
-};
-router.get("/", async (_req, res) => {
-  const users = await User.findAll(userOptions);
+  });
   res.json(users);
 });
 
@@ -49,7 +34,45 @@ router.put("/:username", tokenExtractor, async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id, userOptions);
+  let where = {};
+  if (req.query.read) {
+    let status;
+    if (req.query.read === "false") {
+      status = false;
+    } else if (req.query.read === "true") {
+      status = true;
+    } else {
+      res
+        .status(400)
+        .json({ error: "unknown read status state, use true or false" });
+    }
+    where = { read: { [Op.is]: status } };
+  }
+
+  const user = await User.findByPk(req.params.id, {
+    include: [
+      {
+        model: Blog,
+        attributes: {
+          exclude: ["userId"],
+        },
+      },
+      {
+        model: Blog,
+        as: "reading_list",
+        attributes: { exclude: ["userId"] },
+        through: {
+          attributes: ["id", "read"],
+          as: "status",
+          where,
+        },
+        include: {
+          model: User,
+          attributes: ["name"],
+        },
+      },
+    ],
+  });
   if (user) {
     res.json(user);
   } else {
